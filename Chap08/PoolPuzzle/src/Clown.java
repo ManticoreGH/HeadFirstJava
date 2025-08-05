1dd0ade376f0108da4498478cadd95a4e6d1b7be
Soufflet Sébastien <sebastien.soufflet@gmail.com>
2025-08-05 22:42:17 +0200

    Add Duck and UseADuck classes; implement basic functionality

---
+++ Chap08/PoolPuzzle/src/Clown.java
@@ -0,0 +1,1 @@
+public class Clown extends Picasso {}
---
+++ Chap09/UseADuck/untitled/src/Duck.java
@@ -0,0 +1,11 @@
+public class Duck {
+    int size;
+
+    public Duck() {
+        System.out.println("Quack!");
+    }
+
+    public void setSize(int newSize){
+        size = newSize;
+    }
+}
