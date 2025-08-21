274184790e0920ddf06fd87852ec8c64486c91e2
Soufflet Sébastien <sebastien.soufflet@gmail.com>
2025-08-21 17:32:01 +0200

    Refactor App.java to implement number printing logic and handle exceptions

--- Chap12/PrintColors/MixForLoops/src/App.java
+++ Chap12/PrintColors/MixForLoops/src/App.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        String output = "";
+        for (int i = 1; i < nums.size(); i++) {
+            output += nums.get(i) + " ";
+        }
+        System.out.println(output.trim());
+        //Output: 2 3 4 5
+        output = "";
+        for (Integer num : nums){
+            output += nums + " ";
+        }
+        System.out.println(output.trim());
+        //Output: [1, 2, 3, 4, 5] [1, 2, 3, 4, 5] [1, 2, 3, 4, 5] [1, 2, 3, 4, 5] [1, 2, 3, 4, 5]
+        /* output = "";
+        for(int i = 0; i <= nums.size(); i++) {
+            output += nums.get(i) + " ";
+        } */
+        System.out.println(output.trim());
+        //Output: Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5
+        output = "";
+        for (int i = 0; i <= nums.size(); i++) {
+            output += nums.get(i) + " ";
+        }
+        System.out.println(output.trim());
+
     }
 }
