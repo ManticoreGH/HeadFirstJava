ddd07a01b6e3f0ebd3a1098266ccedddfd07df7b
Soufflet Sébastien <sebastien.soufflet@gmail.com>
2025-08-22 00:04:30 +0200

    Refactor App.java to streamline string limiting logic and remove commented-out code

--- Chap12/LimitWithStream/src/App.java
+++ Chap12/LimitWithStream/src/App.java
@@ -1,12 +1,21 @@
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.*;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         List<String> strings = List.of("I", "am", "a", "list", "of", "strings");
-        Stream<String> stream = strings.stream();
-        Stream<String> limit = stream.limit(4);
+        // Stream<String> stream = strings.stream();
+        // Stream<String> limit = stream.limit(4);
+        // Long result = limit.count();
+        // System.out.println("Result = " + result);
         //System.out.println("Limite = " + limit); //Limite = java.util.stream.SliceOps$1@7344699f
-        System.out.println("Limite = " + limit.toList());
+        /**
+         * compacting the code above in a single statement
+         */
+        List<String> result = strings.stream()
+            .limit(4)
+            .collect(Collectors.toList());
+        System.out.println("Result = " + result);
     }
 }
