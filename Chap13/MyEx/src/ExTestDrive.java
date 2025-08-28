5871f932c3bad47ac6b402f8f85719c2f89b0fce
Soufflet Sébastien <sebastien.soufflet@gmail.com>
2025-08-28 23:02:49 +0200

    Add initial project configuration files and a custom exception class

---
+++ Chap13/MyEx/src/ExTestDrive.java
@@ -0,0 +1,12 @@
+class MyEx extends Exception {
+    public MyEx(String message) {
+        super(message);
+    }
+}
+public class ExTestDrive {
+
+
+    public static void main(String[] args) {
+
+    }
+}
