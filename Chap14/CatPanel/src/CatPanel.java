22268385f26a275e6394cd4d38693bf9183c8b79
Soufflet Sébastien <sebastien.soufflet@gmail.com>
2025-08-30 19:26:36 +0200

    Add CatPanel class to display an image of a cat
    - Created CatPanel class extending JPanel and implementing ImageObserver.
    - Overrode paintComponent method to draw a cat image from a file.
    - Implemented imageUpdate method to satisfy ImageObserver interface.
    - Added main method to set up a JFrame and display the CatPanel.

---
+++ Chap14/CatPanel/src/CatPanel.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import javax.swing.*;
+
+public class CatPanel extends JPanel implements ImageObserver {
+    @Override
+    public void paintComponent(Graphics g) {
+        Image image = new ImageIcon("catzilla.png").getImage();
+        g.drawImage(image, 3, 4, this);
+    }
+    public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+        return false;
+    }
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        frame.add(new CatPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
