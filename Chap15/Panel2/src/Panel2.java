a85a8af542eb01c1e6f37e1f54740b4981c12d39
Soufflet Sébastien <sebastien.soufflet@gmail.com>
2025-09-06 18:19:04 +0200

    Add Panel2 application with GUI layout and buttons

--- Chap15/Panel2/src/Panel2.java
+++ Chap15/Panel2/src/Panel2.java
@@ -1,9 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Panel1 {
+public class Panel2 {
     public static void main(String[] args) throws Exception {
-        Panel1 gui = new Panel1();
+        Panel2 gui = new Panel2();
         gui.go();
     }
 
@@ -11,17 +11,17 @@ public class Panel1 {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         panel.setBackground(Color.darkGray);
-        frame.getContentPane().add(BorderLayout.EAST, panel);
+        
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         
         JButton button = new JButton("shock me");
         JButton buttonTwo = new JButton("bliss");
-        JButton buttonThree = new JButton("huh?");
-
+        
         panel.add(button);
         panel.add(buttonTwo);
-        panel.add(buttonThree);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
 
-        frame.setSize(200, 200);
+        frame.setSize(250, 200);
         frame.setVisible(true);
     }
 }
