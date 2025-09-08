b8bb29e1c2c2d862349288fe65aa8c696deafeba
Soufflet Sébastien <sebastien.soufflet@gmail.com>
2025-09-09 01:16:22 +0200

    Refactor BeatBox.java to improve code readability with enhanced comments and formatting

--- Chap15/BeatBox/src/BeatBox.java
+++ Chap15/BeatBox/src/BeatBox.java
@@ -7,21 +7,27 @@ import javax.swing.*;
 
 public class BeatBox {
 
-    private ArrayList<JCheckBox> checkboxList;
+    private ArrayList<JCheckBox> checkboxList; // We store checkboxes in an ArrayList
     private Sequencer sequencer;
     private Sequence sequence;
     private Track track;
-
+    /*
+     * Names of instruments to be displayed in the GUI
+     */
     String[] instrumentNames = {
-        "Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare",
-        "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo",
-        "Maracas", "Whistle", "Low Conga", "Cowbell",
-        "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga"
+            "Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare",
+            "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo",
+            "Maracas", "Whistle", "Low Conga", "Cowbell",
+            "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga"
     };
-
+    /*
+     * These represent the actual drum "keys". The drum channel is like a piano,
+     * except each "key" on the piano is a different drum. So the number "35"
+     * is the key for the Bass Drum, 42 is Closed Hi-Hat, etc.
+     */
     int[] instruments = {
-        35, 42, 46, 38, 49, 39, 50, 60,
-        70, 72, 64, 56, 58, 47, 67, 63
+            35, 42, 46, 38, 49, 39, 50, 60,
+            70, 72, 64, 56, 58, 47, 67, 63
     };
 
     public static void main(String[] args) throws Exception {
@@ -33,6 +39,11 @@ public class BeatBox {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BorderLayout layout = new BorderLayout();
         JPanel background = new JPanel(layout);
+        /*
+         * An "empty border" gives us a margin between of the panel and where the
+         * components are placed.
+         * Purely aesthetic.
+         */
         background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         Box buttonBox = new Box(BoxLayout.Y_AXIS);
@@ -46,6 +57,9 @@ public class BeatBox {
         buttonBox.add(stop);
 
         JButton upTempo = new JButton("Tempo Up");
+        // Lambda expressions are perfect for these buttons are pressed, all we want to
+        // do is call a specific method.
+        // The default tempo is 1.0f, so we're adjusting +/- 3% per click.
         upTempo.addActionListener(e -> changeTempo(1.03f));
         buttonBox.add(upTempo);
 
@@ -56,6 +70,7 @@ public class BeatBox {
         Box nameBox = new Box(BoxLayout.Y_AXIS);
         for (String instrumentName : instrumentNames) {
             JLabel instrumentLabel = new JLabel(instrumentName);
+            // This border on each instrument name helps them line up with the checkboxes
             instrumentLabel.setBorder(BorderFactory.createEmptyBorder(4, 1, 4, 1));
             nameBox.add(instrumentLabel);
         }
@@ -63,7 +78,8 @@ public class BeatBox {
         background.add(BorderLayout.WEST, nameBox);
 
         frame.getContentPane().add(background);
-
+        // Another layout manager, this one lets you put components in a grid with rows
+        // and columns.
         GridLayout grid = new GridLayout(16, 16);
         grid.setVgap(1);
         grid.setHgap(2);
@@ -86,6 +102,10 @@ public class BeatBox {
     }
 
     private void setUpMidi() {
+        /**
+         * The usual Midi setup stuff for getting a Sequencer, the Sequence,
+         * and the Track. Again, nothing special.
+         */
         try {
             sequencer = MidiSystem.getSequencer();
             sequencer.open();
@@ -98,12 +118,27 @@ public class BeatBox {
     }
 
     private void buildTrackAndStart() {
+        /**
+         * We'll make a 16-element array to hold the values for one instrument,
+         * across all 16 beats. If the instrument is supposed to play on that beat, the
+         * value at that element, will be the key. If that instrument is NOT supposed to
+         * play
+         * on that beat, put in a zero.
+         */
         int[] trackList;
 
+        // Get rid of the old track, make a fresh one
         sequence.deleteTrack(track);
         track = sequence.createTrack();
-
+        // Do this for each of the BEATS for this row
         for (int i = 0; i < 16; i++) {
+            /**
+             * Is the checkbox at this beat selected?
+             * If yes, put the key value in this slot in the array (The slot that represents
+             * this beat).
+             * Otherwise, the instrument is NOT supposed to play at this beat, so set it to
+             * zero.
+             */
             trackList = new int[16];
             int key = instruments[i];
             for (int j = 0; j < 16; j++) {
@@ -114,36 +149,68 @@ public class BeatBox {
                     trackList[j] = 0;
                 }
             }
-            makeTracks(trackList);
+            makeTracks(trackList); // For this instrument, and for all 16 beats, make events and add then to the
+                                   // track.
             track.add(makeEvent(CONTROL_CHANGE, 1, 127, 0, 16));
         }
+        /**
+         * We always want to make sure that there IS an event at beat 16 (it goes 0 to
+         * 15).
+         * Otherwise the BeatBox might not go the full 16 beats before it starts over.
+         */
         track.add(makeEvent(PROGRAM_CHANGE, 9, 1, 0, 15));
         try {
             sequencer.setSequence(sequence);
+            // Lets you specify the number of loop iterations, or in this case, continuous
+            // looping.
             sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
             sequencer.start();
             sequencer.setTempoInBPM(120);
-            sequencer.start();
+            sequencer.start();// Now play the thing.
         } catch (InvalidMidiDataException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * The Tempo Factor scales the sequencer's tempo by the factor provided, slowing
+     * the beat down or speeding it up.
+     * 
+     * @param tempoMultiplier
+     */
     private void changeTempo(float tempoMultiplier) {
         float tempoFactor = sequencer.getTempoFactor();
         sequencer.setTempoFactor(tempoFactor * tempoMultiplier);
     }
 
+    /**
+     * This makes events for one instrument at a time, for all 16 beats. 
+     * So it might get an int[] for the bass drum, and each insdex in the array 
+     * will hold either the key of that instrument or a zero. If it's a zero, 
+     * the instrument isn't supposed to play at that beat.
+     * Otherwise, make an event and add it to the track.
+     * @param list
+     */
     private void makeTracks(int[] list) {
         for (int i = 0; i < 16; i++) {
             int key = list[i];
             if (key != 0) {
+                // Make the NOTE ON and NOTE OFF events, and add them to the track.
                 track.add(makeEvent(NOTE_ON, 9, key, 100, i));
                 track.add(makeEvent(NOTE_OFF, 9, key, 100, i + 1));
             }
         }
     }
-
+    /**
+     * A utility method to make it easier to add events to the track.
+     * 
+     * @param cmd  the command for the event
+     * @param chnl the channel
+     * @param one  the first data value
+     * @param two  the second data value
+     * @param tick the beat (position) on which to insert the event
+     * @return a MidiEvent object
+     */
     private MidiEvent makeEvent(int cmd, int chnl, int one, int two, int tick) {
         MidiEvent event = null;
         try {
